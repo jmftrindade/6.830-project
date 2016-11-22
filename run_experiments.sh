6cc93c199c4998d398575625954c724d81c01665
Joana Trindade <jmftrindade@gmail.com>
2016-11-21 21:05:18 -0500

    Major refactoring, and add experiments code.

---
+++ run_experiments.sh
@@ -0,0 +1,82 @@
+#!/usr/local/bin/bash
+#
+# Requires Bash v4 for associative arrays.
+# Mac install with Homebrew will make it available under /usr/local/bin.
+
+# Globals.
+TODAY=`date "+%Y%m%d"`
+NUMBER_RUNS=1  #10
+
+# Experiment -> dataset directory
+declare -A EXPERIMENT_DATASETS
+#EXPERIMENT_DATASETS["categorical"]=datasets/classification/*csv
+EXPERIMENT_DATASETS["numerical"]=datasets/regression/*csv
+#EXPERIMENT_DATASETS["FD_paper"]=datasets/from_FD_paper/*csv
+
+# Run experiment and generate logs.
+function run_experiment {
+  local input_dataset=$1
+  local output_log_file=$2
+  local number_runs=$3
+
+  echo "input_dataset=$input_dataset"
+  echo "output_log_file=$output_log_file"
+  echo "number_runs=$number_runs"
+
+  args="-m timeit -n 1 -r ${number_runs} 'import os' 'os.system(\"python run_ml_algos.py -f ${input_dataset}\")' > ${output_log_file} 2>&1"
+  echo "Running:"
+  echo "python $args"
+  #eval python $args
+  echo "Saved logs as \"${output_log_file}\"."
+}
+
+# Generate CSV from experiment logs.
+function generate_csv {
+  local input_log_file=$1
+  local output_csv_file=$2
+
+  # Generate CSV from experiment logs.
+  echo "Generating CSV..."
+  echo "grep run_classifier ${input_log_file} | sed -e $'1i\\\nfunction,runtime_seconds,target_num_unique,algo,accuracy' > ${output_csv_file}"
+  #grep run_classifier ${input_log_file} | sed -e $'1i\\\nfunction,runtime_seconds,target_num_unique,algo,accuracy' > ${output_csv_file}
+  echo "Saved CSV as \"${output_csv_file}\"."
+}
+
+# Generates plot from CSV.
+function plot_csv {
+  local input_csv_file=$1
+  local output_plot_file=$2
+  local x_axis_column=$3
+  local y_axis_column=$4
+
+  echo "Plotting results..."
+  echo "python plot_experiment_results.py -i ${input_csv_file} -o ${output_plot_file} -x ${x_axis_column} -y ${y_axis_column}"
+  python plot_experiment_results.py -i ${input_csv_file} -o ${output_plot_file} -x ${x_axis_column} -y ${y_axis_column}
+  echo "Saved plot as \"${output_plot_file}\"."
+}
+
+for experiment in "${!EXPERIMENT_DATASETS[@]}";
+do
+  echo "\nexperiment=$experiment"
+
+  input_datasets_dir=${EXPERIMENT_DATASETS[$experiment]}
+
+  for input_dataset_file in ${input_datasets_dir} ;
+  do
+    file_basename=$(basename $input_dataset_file)
+    file_basename_no_extension=$TODAY-${file_basename%.*}
+    log_filename=experiment_logs/${file_basename_no_extension}.log
+    csv_filename=experiment_logs/${file_basename_no_extension}.csv
+    plot_file_basename=plots/${file_basename_no_extension}
+
+    run_experiment $input_dataset_file $log_filename $NUMBER_RUNS
+    generate_csv $log_filename $csv_filename
+
+    # TODO: Generalize this code.
+    plot_csv $csv_filename $plot_file_basename-algo_vs_runtime.pdf algo runtime_seconds
+    plot_csv $csv_filename $plot_file_basename-num_unique_vs_runtime.pdf target_num_unique runtime_seconds
+    plot_csv $csv_filename $plot_file_basename-algo_vs_accuracy.pdf algo accuracy
+    plot_csv $csv_filename $plot_file_basename-num_unique_vs_accuracy.pdf target_num_unique accuracy
+  done
+
+done
---
+++ run_ml_algos.py
@@ -0,0 +1,182 @@
+import argparse
+import numpy as np
+import pandas as pd
+import random
+import sys
+import time
+from functools import wraps
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+# To convert str to integer (preprocessing.LabelEncoder())
+from sklearn import preprocessing
+
+from sklearn import linear_model, datasets
+from sklearn import svm
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+from sklearn.multiclass import OneVsRestClassifier
+
+
+def fn_timer(function):
+    """
+    Timer decorator that prints total running time of execution.
+
+    A function decorated by this may also provide additional stats via
+    a "additional_timer_stats" kwarg.
+    """
+    @wraps(function)
+    def function_timer(*args, **kwargs):
+        t0 = time.time()
+        result = function(*args, **kwargs)
+        t1 = time.time()
+
+        # First column is time.
+        stats_csv_header = 'function,runtime_seconds'
+        stats_csv_data = '%s,%s' % (function.func_name, str(t1 - t0))
+
+        # Remaining columns.
+        if 'additional_timer_stats' in kwargs:
+            for k, v in kwargs['additional_timer_stats'].iteritems():
+                stats_csv_header += ',%s' % k
+                stats_csv_data += ',%s' % str(v)
+
+        if 'additional_timer_stats_from_result' in kwargs:
+            for stat in kwargs['additional_timer_stats_from_result']:
+                stats_csv_header += ',%s' % stat
+                stats_csv_data += ',%s' % str(result[stat])
+
+        print >> sys.stderr, stats_csv_header
+        print >> sys.stderr, stats_csv_data
+
+        return result
+    return function_timer
+
+
+def get_dataframe_as_float_from_csv(csv_filename):
+    """
+    Returns a dataframe loaded from CSV and parsed as floats.  The reason why
+    we need the columns to be floats is because NaN can only be inserted into
+    float columns.
+
+    TODO: Should we use null instead?  Maybe that doesn't suffer from the NaN
+    problem, where the column dtype needs to be a float.
+    """
+    data = pd.read_csv(csv_filename)
+
+    for col in data:
+        if isinstance(data[col].dtypes, (int)):
+            data[col] = data[col].astype(float)
+
+    return data
+
+
+def encode_target(df, target_column):
+    """Add column to df with integers for the target.
+
+    Args
+    ----
+    df -- pandas DataFrame.
+    target_column -- column to map to int, producing
+                     new Target column.
+
+    Returns
+    -------
+    df_mod -- modified DataFrame.
+    targets -- list of target names.
+    """
+    df_mod = df.copy()
+    targets = df_mod[target_column].unique()
+    map_to_int = {name: n for n, name in enumerate(targets)}
+    # FIXME: Is this replacing the values in the original column?
+    df_mod["Target"] = df_mod[target_column].replace(map_to_int)
+
+    return (df_mod, targets)
+
+
+def run_all_classifiers(targ, features, df):
+    """Try all classification ML algorithms and report their accuracies."""
+
+    df2, targets = encode_target(df, targ)
+
+    y = df2["Target"]
+    X = df2[features]
+
+    # All classifiers that we consider.
+    # TODO: Optionally investigate using GridSearch if the accuracies end up
+    # being too low.
+    classifiers = [
+        {'name': 'Logistic Regression',
+         'dt': linear_model.LogisticRegression()},
+        {'name': 'Decision Tree',
+         'dt': DecisionTreeClassifier(min_samples_split=30, random_state=99)},
+        {'name': 'SVM',
+         'dt': svm.SVC(decision_function_shape='ovo')}
+    ]
+
+    # Experiment stats to record per classifier run.
+    fn_stats_to_record = {
+        'target_num_unique': len(y.unique())
+    }
+    fn_stats_to_record_from_result = ['accuracy']
+
+    for classifier in classifiers:
+        fn_stats_to_record['algo'] = classifier['name']
+        res = run_classifier(
+            y, X, classifier['dt'],
+            # NOTE: This is intentional, as these need to be kwargs.
+            additional_timer_stats=fn_stats_to_record,
+            additional_timer_stats_from_result=fn_stats_to_record_from_result)
+        #print('%s classifier accuracy = %f' % (
+        #    classifier['name'], res['accuracy']))
+
+
+@fn_timer
+def run_classifier(y, X, dt, *args, **kwargs):
+    le = LabelEncoder()
+    for col in X.columns.values:
+        data = X[col]
+        le.fit(data.values)
+        X[col] = le.transform(X[col])
+    dt.fit(X, y)
+    expected = y
+    predicted = dt.predict(X)
+
+    # Organized as named entries in a dict for stats collection.
+    return {'accuracy': metrics.accuracy_score(expected, predicted)}
+
+
+def run_ml_for_all_columns(df):
+    # FIXME: I don't think we can figure out an arbitrary "threshold" for this.
+    # We should just run both classification and regression for numerical
+    # columns, and record how many unique values we have.
+    #threshold = 30
+
+    for col in df:
+        print('\nRunning ML for column \"%s\"' % col)
+        features = [column for column in df.columns if column is not col]
+
+        # Use both classification and regression for numerical data.
+        # All numerical data is read as floats, so no need to check for other
+        # numerical data types here.
+        if df[col].dtypes == 'float64':
+            print('TODO: check accuracy for regression.')
+
+        run_all_classifiers(col, features, df)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Runs classification and regression algos over an input dataset.')
+    parser.add_argument(
+        '-f',
+        '--input_csv_file',
+        help='Relative path of input CSV file containing data set with numeric'
+        ' columns.',
+        required='True')
+    args = parser.parse_args()
+
+    print('Running ML algos for input dataset \"%s\"' % args.input_csv_file)
+
+    df = get_dataframe_as_float_from_csv(args.input_csv_file)
+    run_ml_for_all_columns(df)
